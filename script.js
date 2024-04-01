256977bb7197a536ce273bda4404f81c19a55e0f
VishalBharathi <vishalbarti369@gmail.com>
2024-04-01 15:34:56 +0530

    files were added

---
+++ script.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './crud operations/src/App.js';
+
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
